a701f3acd903532817b47de466532ea7ca986c47
AikhanHabibov <aikhan.habibov@gmail.com>
2024-07-23 16:25:42 +0200

    gvallery
    efedf

--- script.js
+++ script.js
@@ -43,5 +43,31 @@
 //let answer = confirm("stul");
 //alert(answer);
 
-const claculate3 = (a, b) => a + b;
-console.log(claculate3(1, 3));
+//const claculate3 = (a, b) => a + b;
+//console.log(claculate3(1, 3));
+
+// Список посилань на зображення
+const imageUrls = [
+  "https://img.freepik.com/free-photo/travel-destination-view-from-airplane-window_23-2151382931.jpg",
+  "https://img.freepik.com/free-photo/beautiful-girl-standing-viewpoint-koh-nangyuan-island-near-koh-tao-island-surat-thani-thailand_335224-1094.jpg",
+  "https://img.freepik.com/free-photo/3d-icon-travel-with-airplane_23-2151037350.jpg",
+  "https://img.freepik.com/free-photo/travel-concept-with-baggage_23-2149153260.jpg",
+  "https://img.freepik.com/free-photo/maldives-island_1203-7338.jpg",
+  "https://img.freepik.com/free-photo/young-female-traveler-enjoying-rural-surroundings_23-2149125532.jpg",
+  "https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg",
+];
+// Функція для створення розмітки галереї
+function createImageGallery(urls) {
+  const gallery = document.getElementById("imageGallery");
+  urls.forEach((url) => {
+    const listItem = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = url;
+    img.alt = "Image";
+    listItem.appendChild(img);
+    gallery.appendChild(listItem);
+  });
+}
+
+// Виклик функції для створення галереї
+createImageGallery(imageUrls);
